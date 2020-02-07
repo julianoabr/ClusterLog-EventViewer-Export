bf489f5a778a04ce16c8479481cc3ec4181926ba
Juliano Ribeiro <35538913+julianoabr@users.noreply.github.com>
2020-02-07 11:44:44 -0300

    Update GenerateClusterLogEventVwr.ps1

--- Script/GenerateClusterLogEventVwr.ps1
+++ Script/GenerateClusterLogEventVwr.ps1
@@ -1,7 +1,7 @@
 #Requires -RunAsAdministrator
 #Requires -Version 3.0
 <#
-.Synopsis:   Script to export Event Logs and Cluster Log to a shared folder
+.Synopsis:   Script to Eexport Event Logs and Cluster Log to a shared folder
 .Created by: Juliano Alves de Brito Ribeiro (julianoalvesbr@live.com)
 .Version:    0.1
 .Requirements:
@@ -11,14 +11,14 @@
 # Config
 $logFileList = @()
 $logFileList = "Application","System","Security" # Add Name of the Logfile (System, Application, etc)
-$outputpath = "C:\temp\" # Add Path, needs to end with a backsplash
-[string]$PCname = $env:COMPUTERNAME
+$outputpath = "$Env:SystemDrive\temp\" # Add Path, needs to end with a backsplash
+[string]$PCname = $Env:COMPUTERNAME
 
 
 if (Test-Path $outputpath){
-    Write-Output "O caminho para salvar o arquivo já existe. Continuarei o script..."
+    Write-Output "Path to save temporary files already exist. Let's continue..."
 }else{
-    Write-Output "O caminho para salvar o arquivo não existe. Criarei...."
+    Write-Output "Path to save temporary files does not exists. I will create It"
     New-Item -Path "c:\" -Name "Temp" -ItemType "Directory" -Force -Verbose 
 
 }
@@ -51,7 +51,7 @@ Write-Output "Would you like to delete old evtx files in path $outputpath before
 #Export Event Viewer Log Files
 foreach ($logFileName in $logFileList)
     {
-    Write-Output "Gerando os logs de $logFileName ..."
+    Write-Output "Generating log: $logFileName ..."
     Start-Sleep -Seconds 2
     $exportFileName = $logFileName + "-" + $PCname + (get-date -f yyyyMMdd) + ".evtx"
     $logFile = Get-WmiObject -Class Win32_NTEventLogFile | Where-Object -FilterScript {$_.logfilename -eq $logFileName}
@@ -100,6 +100,8 @@ New-Item -Path \\$SharedPC\$DriveLetter$\$Path -ItemType Directory -Name $PCname
 Start-Sleep -Seconds 3 -Verbose
 
 Set-Location "$env:SystemDrive\TEMP"
+
+Write-Host "I will copy all the logs to a shared folder now" -ForegroundColor White -BackgroundColor DarkGreen
 
 Move-Item -Path ".\*.log" -Destination \\$SharedPC\$DriveLetter$\$Path\$PCName -Verbose -Force
 Move-Item -Path ".\*.evtx" -Destination \\$SharedPC\$DriveLetter$\$Path\$PCName -Verbose -Force
